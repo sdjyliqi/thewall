3055242805bc27f37bcc6f165f2b506cae6f3925
liqi <sdjyliqi@163.com>
2022-01-18 21:54:00 +0800

    +redis

--- utils/redis.go
+++ utils/redis.go
@@ -5,11 +5,35 @@ import (
 	"fmt"
 	"github.com/go-redis/redis/v8"
 	"github.com/golang/glog"
+	"net/url"
+	"regexp"
+	"strconv"
 )
 
 var (
 	redisClient *redis.Client
 )
+
+func safeRedisAddr(addr string) string {
+	return regexp.MustCompile(`://([^:]+):(.*)@`).ReplaceAllString(addr, "://$1:****@")
+}
+
+// redis://db:password@host:port
+func parseRedisAddr(addr string) (host string, password string, db int) {
+	db = 0
+	u, err := url.Parse(addr)
+	if err != nil {
+		host = addr
+	} else {
+		host = u.Host
+		db64, _ := strconv.ParseInt(u.User.Username(), 0, 32)
+
+		db = int(db64)
+		password, _ = u.User.Password()
+	}
+	glog.V(8).Infof("parse redis URI. addr = %s, host = %s, db = %d", safeRedisAddr(addr), host, db)
+	return
+}
 
 //InitRedis ...初始化redis，记得要先解密
 func InitRedis(addr string, password string) (*redis.Client, error) {
@@ -19,7 +43,6 @@ func InitRedis(addr string, password string) (*redis.Client, error) {
 		Password: password,
 		DB:       0, // use default DB
 	})
-
 	_, err := redisClient.Ping(context.Background()).Result()
 	if err != nil {
 		glog.Infof("[init] Initialize redis client failed,please check the addr:%+v,password:%+v,err:%+v", addr, password, err)
--- utils/redis_test.go
+++ utils/redis_test.go
@@ -9,6 +9,8 @@ import (
 func Test_Redis(t *testing.T) {
 	key, value := "name", "liqi"
 	client := GetRedisClient()
+	set := client.Set(context.Background(), key, value, 0)
+	t.Log(set)
 	result := client.Get(context.Background(), key)
 	assert.Equal(t, value, result.Val())
 }
